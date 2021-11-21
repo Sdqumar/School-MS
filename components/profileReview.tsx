abebf4dda036bfdf36048c8cc2d68d057f9361ef
Sdqumar <sdqumar09@gmail.com>
2021-11-21 15:08:40 +0100

    find result admin page

--- components/profileReview.tsx
+++ components/profileReview.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import useStore from "./useStore";
 
-export default function ProfileReview() {
-    const user = useStore((state) => state.user);
+export default function ProfileReview({user}) {
   
     const userProfile = [
       { name: "Admission-No", value: user?.admissionNo },
@@ -14,12 +12,12 @@ export default function ProfileReview() {
   
     return (
         <main>
-          <section className=" align-center border-2 border-blue-300 border-solid flex mt-14 h-44">
+          <section className=" align-center border border-blue-300 border-solid flex mt-14 h-44">
             <div className="flex flex-col">
               {userProfile.map((item) => (
                 <div
                   className="flex children:w-40 h-screen 
-              border-solid  border-b-2 font-medium"
+              border-solid  border-b font-medium"
                 >
                   <span className="text-center bg-blue-200   last:border-0">
                     {item.name}
