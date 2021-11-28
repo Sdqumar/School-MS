540bef20c5a3983ccf78a55b0797229285350bc0
Sdqumar <sdqumar09@gmail.com>
2021-11-28 22:50:06 +0100

    init Flutterwave

--- config/connectDB.ts
+++ config/connectDB.ts
@@ -1,19 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = handler => async (req, res) => {
-   
+const connectDB = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
   try {
-    
-  
-  await mongoose.connect(process.env.DB_URL, {})
-    return handler(req, res)
+    await mongoose.connect(process.env.DB_URL, {});
+    return handler(req, res);
   } catch (error) {
-   console.log(error);
+    console.log(error);
   }
-  
-  }
+};
 
 export default connectDB;
